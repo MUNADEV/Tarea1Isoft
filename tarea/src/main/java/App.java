268d23d5e9729a0abd08b48738a2345070f2a5d3
Ricardo Millanao Burgos <r.millanao@ufromail.cl>
2021-08-28 22:18:45 -0400

    Aplicando CSV pre funcional

--- tarea/src/main/java/App.java
+++ tarea/src/main/java/App.java
@@ -7,10 +7,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import controller.*;
 public class App {
+
 
     public static void main(String[] args) {
 
+        AppController ap = new AppController();
+        ap.writeData1();
 
     }
 }
--- tarea/src/main/java/controller/AppController.java
+++ tarea/src/main/java/controller/AppController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
 import model.Usuario;
 
@@ -16,19 +19,45 @@ public class AppController {
     ArrayList<Usuario> listaUsuario1 = new ArrayList<>();
     ArrayList<Usuario> listaUsuario2 = new ArrayList<>();
 
+    String rutaData1 = "\\C:\\Users\\javie\\Proyectos\\tarea_2s2021\\enunciado\\datasets\\dataset1.csv";
+    String rutaData2 = "\\C:\\Users\\javie\\Proyectos\\tarea_2s2021\\enunciado\\datasets\\dataset2.csv";
 
     public AppController() {
     }
 
     public void writeData1(){
 
+        try {
+            // Create an object of file reader class with CSV file as a parameter.
+            FileReader filereader = new FileReader(rutaData1);
 
-        try (CSVReader reader = new CSVReader(new FileReader("C:\\Users\\javie\\Proyectos\\tarea_2s2021\\enunciado\\datasets\\dataset1.csv"))) {
-            List<String[]> r = reader.readAll();
-            r.forEach(x -> System.out.println(Arrays.toString(x)));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException | CsvException e) {
+            // create csvParser object with
+            // custom separator semi-colon
+            CSVParser parser = new CSVParserBuilder().withSeparator(' ').build();
+
+            // create csvReader object with parameter
+            // filereader and parser
+            CSVReader csvReader = new CSVReaderBuilder(filereader)
+                    .withCSVParser(parser)
+                    .build();
+
+            // Read all data at once
+            List<String[]> allData = csvReader.readAll();
+
+            // Print Data.
+            int i = 0;
+            for (String[] row : allData) {
+
+
+                for (String cell : row) {
+                    listaUsuario1.set
+                    System.out.print(cell);
+                }
+                i++;
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
